49c1bc1a7a4d9ed8143d820075326f5369c65388
GeekTree0101 <h2s1880@gmail.com>
2016-11-16 17:51:36 +0900

    update

---
+++ arduino(building)/airpointer/airpointer.ino
@@ -0,0 +1,224 @@
+/***********************************
+    Title : MIRO airpointer project
+    Copyright(C) 2016 MIRO KyongPook Univ
+     
+    하현수, 박민규, 황인득,  이재훈, 이동은
+    update : 2016.11.15. 22:05
+***********************************/
+
+/**********************************
+       [+] Import Library
+**********************************/
+
+#include <Wire.h>
+#include <MPU6050.h>
+#include <stdio.h>
+/**********************************
+    [+] Global & Const Variables
+**********************************/
+
+#define Drawing_button 7                //Button Pin (PULL-UP Button)
+#define Click_button 8
+#define ZoomIn_button 9
+#define Motion_button 10
+#define Lazer_button 11
+
+#define key_press_delay 30              //key & Mouse Motion delay time
+#define mouse_press_delay 50
+#define Hardware_delay 10               //Hardware Delay
+
+// Data protocol
+#define DATA_MOTION 1
+#define DATA_ZOOMIN 2
+#define DATA_DRAWING 3
+#define DATA_PASSPAGE 4
+
+/**********************************
+        [+] Function
+***********************************/
+
+void Button_setup();                    //Pull-up Digital Button
+void MPU_setup();                       //MPU6050 Init Setup
+short Check_X(int Data);                //Calculate X position
+short Check_Y(int Data);                //Calculate Y position
+
+boolean drawing_flag = false;
+boolean zoomin_flag = false;
+
+void setup(){                               //Hardware Setup
+
+    Serial1.begin(9600);                     //bluetooth serial 9600 baudrate
+    Button_setup();
+    MPU_setup();
+    delay(2000);
+    
+}
+
+void loop(){                                //Main Loop Proc
+
+    //NOTE : Must import Low Battery System Cuz, overhead very big in this loop
+    
+    int16_t* Data_Stack;
+    Data_Stack = (int16_t*)malloc(7*sizeof(int16_t));
+    
+
+    while(1){
+        
+        short X = 0;
+        short Y = 0;
+        char buff[7];
+
+        Wire.beginTransmission(0x68);         //Begin MPU
+        Wire.write(0x3B);
+        Wire.endTransmission(false);             //Sustain connection
+        Wire.requestFrom(0x68, 14, true);
+
+        Data_Stack[0] = Wire.read() << 8 | Wire.read();  // X acc data
+        Data_Stack[1] = Wire.read() << 8 | Wire.read();  // Y acc data
+        Data_Stack[2] = Wire.read() << 8 | Wire.read();   // Z acc data
+        Data_Stack[3] = Wire.read() << 8 | Wire.read();   // Temp
+        Data_Stack[4] = Wire.read() << 8 | Wire.read();   // X gyro data select
+        Data_Stack[5] = Wire.read() << 8 | Wire.read();   // Y gyro data
+        Data_Stack[6] = Wire.read() << 8 | Wire.read();   // Z gyro data select
+
+        X = Check_X(Data_Stack[6]);
+        Y = Check_Y(Data_Stack[4]);
+        
+
+        if(digitalRead(Click_button) == LOW){              //Click Function
+            
+            sprintf(buff, "%d/%d/%d/&",4,X,Y);
+            Serial1.println(buff); 
+
+            zoomin_flag = ~zoomin_flag + 2;
+
+            delay(1000);
+        }
+
+        if(digitalRead(Drawing_button) == LOW){
+
+            sprintf(buff, "%d/%d/%d/&",3,X,Y);
+            Serial1.println(buff); 
+
+            drawing_flag = ~drawing_flag + 2;
+
+            delay(1000);
+            
+        }
+
+        if(digitalRead(ZoomIn_button) == LOW ){             //ZoomIn Function
+
+            sprintf(buff, "%d/%d/%d/&",2,X,Y);
+            Serial1.println(buff); 
+            delay(1000);       
+            
+        }
+
+        if(digitalRead(Motion_button) == LOW){              //Motion Control
+
+            sprintf(buff, "%d/%d/%d/&",1,X,Y);
+            Serial1.println(buff); 
+        }
+        else{
+        
+            if( drawing_flag == true || zoomin_flag == true ){
+
+            sprintf(buff, "%d/%d/%d/&",7,X,Y);
+            Serial1.println(buff); 
+            }
+        }
+    }
+  
+    free(Data_Stack);                                     // Data-Stack memory free
+}
+
+
+void Button_setup(){                                      //Pull-up Digital Button;
+
+    //FIXME : Must script base on DDR, PORT register
+
+    pinMode(Drawing_button,INPUT);  //DDRE.6 0b 0X00 0000 X:1  (1 = INPUT)
+    pinMode(Click_button ,INPUT);   //DDRB.4 0b 000X 0000 X:1
+    pinMode(ZoomIn_button,INPUT);   //DDRB.5 0b 00X0 0000 X:1
+    pinMode(Motion_button,INPUT);   //DDRB.6 0b 0X00 0000 X:1
+    pinMode(Lazer_button,OUTPUT);   //DDRB.7 0b X000 0000 X:0  (0 = OUTPUT)
+    
+    //DDRE = 0x40;
+    //DDRB = 0x70;  
+
+    digitalWrite(Drawing_button,HIGH); //PORTE.6 0b0X00 0000 X:1
+    digitalWrite(Click_button ,HIGH);  //PORTB.4
+    digitalWrite(ZoomIn_button,HIGH);  //PORTB.5
+    digitalWrite(Motion_button,HIGH);  //PORTB.6 0bXXXX 0000 X:1 (1 = HIGH)
+
+    //PORTB = 0xF0;
+    //PORTE = 0x40;
+}
+
+void MPU_setup(){                           //MPU6050 Init Setup
+
+    Wire.begin();
+    Wire.beginTransmission(0x68);
+    Wire.write(0x6B);
+    Wire.write(0);
+    Wire.endTransmission(true);
+    delay(30);
+}
+
+short Check_X(int Data) {
+
+            short Xval;  
+
+            if(Data > 1000) {
+
+              Xval = -2-Data / 1000;
+            }
+            else if( Data < -1000) {
+            
+              Xval = 2-Data / 1000;
+            }
+            else if(Data > 500 && Data < 1000)  {                
+              
+              Xval = -3;               
+            }
+            else if(Data > -1000 && Data < -500) {
+              
+              Xval = 3;
+            }
+            else {
+                
+              Xval = 0;
+            }
+            
+
+            return Xval;
+}
+
+
+short Check_Y(int Data) {
+  
+            short Yval;
+
+            if(Data > 1000) {
+            
+              Yval = Data / 1000;
+            }
+            else if(Data < -1000){
+            
+              Yval = Data / 1000;
+            }
+            else if(Data > 500 && Data < 1000)  {                
+              
+              Yval = 1;               
+            }
+            else if(Data > -1000 && Data < -500) {
+                
+                Yval = -1;
+             }
+            else {
+                
+                Yval = 0;
+            }
+
+            return Yval;
+ }
---
+++ arduino(building)/bridge/bridge.ino
@@ -0,0 +1,264 @@
+/***********************************
+    Title : MIRO airpointer project
+    Copyright(C) 2016 MIRO KyongPook Univ
+     
+    하현수, 박민규, 황인득,  이재훈, 이동은
+    update : 2016.11.16 15:44
+***********************************/
+
+
+/**********************************
+       [+] Import Library
+**********************************/
+
+#include <Mouse.h>                            // Mouse lib
+#include <Keyboard.h>                         // Keyboard lib
+#include <unistd.h>                           // string to int lib
+
+/**********************************
+    [+] Global & Const Variables
+**********************************/
+
+//function state data
+#define DATA_MOTION 1
+#define DATA_ZOOMIN 2
+#define DATA_DRAWING 3
+#define DATA_PASSPAGE 4
+
+// HID delay
+#define key_press_delay 30             
+#define mouse_press_delay 50
+
+// Process control flag
+boolean Drawing_flag = false;                   // Drawing Flag
+boolean Zoom_flag = false;                      // Zoom Flag
+boolean Serial_control_flag = false;            // received serial data control flag
+boolean mouse_press_flag = false;
+
+// Global variable
+unsigned int function_state = 99;               // function state base on state data
+short X = 0;                                    // HID mouse X position
+short Y = 0;                                    // HID mouse Y position
+
+/**********************************
+        [+] Function
+***********************************/
+
+void Mouse_interface_setup();                   //Mouse Init Setup
+void Keyboard_interface_setup();                //Keyboard Init Setup
+
+//Zoom function
+void ZoomIn_start();
+void ZoomIn_cancel();
+void ZoomIn_event();
+
+//Drawing function
+void Drawing_start();
+void Drawing_cancel();
+void Drawing_event();
+
+void setup(){                                  //Hardware Setup
+
+    //NOTE : arduino micro(atmega32u4) using Serial1 
+
+    Serial1.begin(9600);
+    Mouse_interface_setup();
+    Keyboard_interface_setup();
+}
+
+void loop(){                                   // Main Loop Proc
+
+    char node[4] ={ ' ' };                     // char buffer
+    
+    unsigned short node_index = 0;
+    unsigned short output_pos = 0;
+
+    while(1){
+    
+    if(Serial1.available()){
+      
+           
+        String str = Serial1.readString();
+
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++){
+
+            if(str[i] == '/'){
+                count++;
+                continue;
+            }
+
+            if(count == 0){
+                function_state = str[i];
+            }
+            else if(count == 1){
+                X = str[i];
+            }
+            else if(count == 2){
+                Y = str[i];
+                function_state = true;
+                break;
+            }
+        }
+                                
+    }                               
+
+    
+    if (Serial_control_flag) {                 // Reality HID control section
+        
+        switch(function_state){
+
+            // Zoomin control section
+            case DATA_ZOOMIN:  
+                
+                ZoomIn_event();                // Zoomin flag toogle function
+                
+                if(Zoom_flag == true){      
+                    ZoomIn_start();
+                }
+                else{
+                    ZoomIn_cancel();
+                }
+                break;
+
+            // Drawing control section
+            case DATA_DRAWING:
+
+                Drawing_event();               // Drawing flag toogle function
+            
+                if(Drawing_flag == true){
+                    Drawing_start();
+                }
+                else{
+                    Drawing_cancel();
+                } 
+
+                break;
+
+            // Passpage control section
+            case DATA_PASSPAGE:
+
+                Mouse.click(MOUSE_LEFT);
+                delay(500);
+                break;
+
+            // Motion controll section
+            case DATA_MOTION:                                       
+
+                if(Drawing_flag == true && mouse_press_flag == false){
+
+                  Mouse.press(MOUSE_LEFT);
+                  mouse_press_flag = ~mouse_press_flag + 2;
+                }
+                else if(Zoom_flag == true && mouse_press_flag == false){
+
+                  Mouse.press(MOUSE_LEFT);
+                  mouse_press_flag = ~mouse_press_flag + 2;
+                }
+                
+                Mouse.move(X,Y,0);
+
+              break;
+
+            default :
+
+                if( Drawing_flag == true || Zoom_flag == true ){     //Zoom or Drawing active state
+
+                    if(mouse_press_flag == true){
+
+                        Mouse.release(MOUSE_LEFT); 
+                        mouse_press_flag = false;
+                    }
+
+                    
+                    Mouse.move(X,Y,0);
+                }
+                else{
+
+                    if(mouse_press_flag == true){
+                        
+                        Mouse.release(MOUSE_ALL);
+                        mouse_press_flag = false;
+                    }
+
+                    
+                }
+                
+        }
+
+        function_state = 99;
+    } 
+}
+
+void Drawing_event(){                       //Drawing event Function
+
+  if(Zoom_flag == false){
+    Drawing_flag = ~Drawing_flag + 2; 
+  }
+
+}
+
+void ZoomIn_event() {                     //Zoomin function toggling
+  
+  if(Drawing_flag == false){
+     Zoom_flag = ~Zoom_flag + 2;
+  }
+  
+}
+
+void Drawing_start() {                   //Drawing start function
+  
+    Keyboard.press(KEY_LEFT_CTRL);
+    delay(key_press_delay);
+    Keyboard.press('p');
+    delay(key_press_delay);
+    Keyboard.release('p');
+    delay(key_press_delay);
+    Keyboard.release(KEY_LEFT_CTRL);
+    delay(key_press_delay);
+    
+    delay(30);
+}
+
+
+void Drawing_cancel() {                  //Drawing cancel function
+  
+    Keyboard.press(KEY_LEFT_CTRL);
+    delay(key_press_delay);
+    Keyboard.press('a');
+    delay(key_press_delay);
+    Keyboard.release('a');
+    delay(key_press_delay);
+    Keyboard.release(KEY_LEFT_CTRL);
+    delay(key_press_delay);
+
+    delay(30);
+}
+
+void ZoomIn_start(){                       //Zoomin event Function
+
+     Keyboard.press(KEY_LEFT_CTRL);
+     delay(key_press_delay);
+     Mouse.move(0,0,3);
+     delay(key_press_delay);
+     Keyboard.release(KEY_LEFT_CTRL);
+     
+}
+
+void ZoomIn_cancel() {
+    Keyboard.write(0x30);
+    delay(key_press_delay);
+}
+
+void Mouse_interface_setup(){               //Mouse Init Setup
+
+    Mouse.begin();
+    delay(30);
+}
+
+void Keyboard_interface_setup(){            //Keyboard Init Setup
+
+    Keyboard.begin();
+    delay(30);
+}
